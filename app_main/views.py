b55017b1cfa1102e9693d4ce1db632856c374c40
nao <nao.kitic@mksp.si>
2026-04-18 16:48:49 +0200

    started making receipt scanning

--- app_main/views.py
+++ app_main/views.py
@@ -1,5 +1,77 @@
 from django.shortcuts import render
+from datetime import datetime
+
+from django.core.serializers.json import DjangoJSONEncoder
+from .models import ReceiptTransaction, Category, ItemTransaction
+
+import json
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def process_receipt_image(request):
+    uploaded_file = None
+    
+    is_Image = False
+    if 'image' in request.FILES:
+        uploaded_file = request.FILES['image']
+        is_Image = True
+    else:
+        uploaded_file = request.FILES['video']
+     
+    curr_user = request.user   
+    context = {}
+   
+    now = datetime.now()
+    now_string = now.strftime("%y%m%d%H%M")
+    new_receipt = ReceiptTransaction.objects.create(title=now_string, user=curr_user)
+    new_receipt.file.save(uploaded_file.name, uploaded_file, save=True)
+    new_receipt_id = str(new_receipt.pk)
+     
+    image_bytes = None
+    with open(new_receipt.file.path, "rb") as f:
+        image_bytes = f.read()
+    
+    existing_categories = Category.objects.all().values('title')
+    categories_string = json.dumps(list(existing_categories), indent=2, cls=DjangoJSONEncoder)
+    
+    prompt_text = (
+        "Extract date, merchant, name, prices and pick a category from "
+       + categories_string
+       + "if it fits to any of them, otherwise create a new one."
+       + " in json format in english in this order, named lower case."
+       + "Add a field 'existing category'"
+       + "and put True if you picked from list and False if you made a new one." 
+       + "Convert money to euro, divide each item."
+       + "The date shoud be in a %Y-%m-%d format, the fields should be empty if no information present"
+    )
+    prompt_contents = None
+    if isImage:
+        prompt_contents = [
+            types.Part.from_bytes(
+                data=image_bytes,
+                mime_type="image/jpg"
+            ),
+            prompt_text
+        ]
+    else:
+        prompt_contents = [
+            types.Part.from_bytes(
+                data=image_bytes,
+                mime_type="video/mp4"
+            ),
+            prompt_text
+        ]
+    
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=prompt_contents
+    )
+    
+    clean_content = response.text.replace("```json", "").replace("```", "").strip()
+    data = json.loads(clean_content)
+    for item in data:
+        item_dt = datetime.strptime(item['date'], "%Y-%m-%d")
+        new_item = ItemTransaction.objects.create()
+    pass
